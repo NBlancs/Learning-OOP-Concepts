5e9da0de5e8bb58acbafe0f8395acd90b2ce1869
noel <bnoel.omar@gmail.com>
2024-09-09 21:36:33 +0800

    update

--- FreeStyleCoding/LoopingArrays/looparr.java
+++ FreeStyleCoding/LoopingArrays/looparr.java
@@ -1,16 +1,15 @@
+package FreeStyleCoding.LoopingArrays;
+
+
 public class looparr {
     public static void main(String[] args) {
         
         String [] cars = {"Toyota", "Vios", "Hilux", "Navarra" };
 
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println(cars[i]);
+        for (String car : cars) {
+            System.out.println(car);
 
         }
 
-
-
     }
 }
-
-
